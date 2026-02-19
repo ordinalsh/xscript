bd22cf1631dbae2b0f95d195fc3fab7c758fe2e4
ordinalsh <ordinadorsuperior@gmail.com>
2026-02-19 12:06:26 +0100

    Añadir operaciones aritmeticas

--- runtime.py
+++ runtime.py
@@ -1,7 +1,9 @@
 
 class RuntimeScript(object):
     def __init__(self):
-        self.defines = {}
+        self.defines = {
+            
+        }
         self.native_calls = {}
 
     def evaluate(self, ast_list: list):
@@ -18,14 +20,36 @@ class RuntimeScript(object):
             expr_type = expr[0]
             if expr_type == "reference":
                 if expr[1] in self.defines:
-                    return self.defines[expr[1]]
+                    return self.defines[expr[1]]["value"]
                 else: return None
+            elif expr_type == "add":
+                return self.evaluate_expression(expr[1]) + self.evaluate_expression(expr[2])
+            elif expr_type == "sub":
+                return self.evaluate_expression(expr[1]) - self.evaluate_expression(expr[2])
+            elif expr_type == "mul":
+                return self.evaluate_expression(expr[1]) * self.evaluate_expression(expr[2])
+            elif expr_type == "div":
+                return self.evaluate_expression(expr[1]) / self.evaluate_expression(expr[2])
+
+    def evaluate_args(self, args:list):
+        args_list = []
+
+        for arg in args:
+            args_list.append(self.evaluate_expression(arg))
+        
+        return args_list
 
     def set(self, define, value, **_): 
-        self.defines[define[1]] = self.evaluate_expression(value)
+        self.defines[define[1]] = {"type": "variable", "value": self.evaluate_expression(value)}
 
     def call(self, function, arguments, **_): 
-        print(function, self.evaluate_expression(arguments[0]))
+        print(function, self.evaluate_args(arguments))
 
 rs = RuntimeScript()
-rs.evaluate([{'op': 'set', 'define': ('reference', 'a'), 'value': 10}, {'op': 'call', 'function': ('reference', 'print'), 'arguments': [('reference', 'a')]}])
+rs.evaluate([
+    {'op': 'set', 'define': ('reference', 'a'), 'value': 10},
+    {'op': 'set', 'define': ('reference', 'b'), 'value': 20},
+    {'op': 'set', 'define': ('reference', 'c'), 'value': ('add', ('reference', 'a'), ('reference', 'b'))},
+    {'op': 'set', 'define': ('reference', 'd'), 'value': ('add', ('reference', 'a'), ('add', ('reference', 'b'), ('mul', ('reference', 'c'), 2)))},
+    {'op': 'call', 'function': ('reference', 'print'), 'arguments': [('reference', 'a'), ('reference', 'b'), ('reference', 'c'), ('reference', 'd')]}
+])
